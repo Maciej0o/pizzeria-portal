b326155d8d7b7bd98fe01b0ea1513030b5e5a147
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-06-21 12:37:00 +0200

    Add Link to Waiter.js and Tables.js

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import styles from './Tables.module.scss';
+import { Link } from 'react-router-dom';
+
 
 const Tables = () => (
   <div className={styles.component}>
     <h2>Tables view</h2>
+    <Link exact to={`${process.env.PUBLIC_URL}/booking/new`} >New Booking </Link>
+    <Link exact to={`${process.env.PUBLIC_URL}/booking/123abc`} >Booking 123abc </Link>
+    <Link exact to={`${process.env.PUBLIC_URL}/event/new`} >New Event </Link>
+    <Link exact to={`${process.env.PUBLIC_URL}/event/123abc`} >Event 123abc </Link>
   </div>
 );
 
