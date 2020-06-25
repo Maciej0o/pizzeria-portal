bd890dd1432a06779867fed241ddab3d6b2fb538
Maciej Smolarczyk <smolarczyk.maciek@gmail.com>
2020-06-25 21:40:20 +0200

    Add materialUI to Tables.js

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,16 +1,91 @@
 import React from 'react';
 import styles from './Tables.module.scss';
-import { Link } from 'react-router-dom';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import Button from '@material-ui/core/Button';
 
+const demoContent = [
+  {id: '1', status: 'free', order: '12 min'},
+  {id: '2', status: 'thinking', order: '33 min'},
+  {id: '3', status: 'ordered', order: '2 min'},
+  {id: '4', status: 'prepared', order: '12 min'},
+  {id: '5', status: 'delivered', order: '20 min'},
+  {id: '6', status: 'paid', order: '8 min'},
+];
+
+const renderActions = status => {
+  switch (status) {
+    case 'free':
+      return (
+        <>
+          <Button>thinking</Button>
+          <Button>new order</Button>
+        </>
+      );
+    case 'thinking':
+      return (
+        <Button>new order</Button>
+      );
+    case 'ordered':
+      return (
+        <Button>prepared</Button>
+      );
+    case 'prepared':
+      return (
+        <Button>delivered</Button>
+      );
+    case 'delivered':
+      return (
+        <Button>paid</Button>
+      );
+    case 'paid':
+      return (
+        <Button>free</Button>
+      );
+    default:
+      return null;
+  }
+};
 
 const Tables = () => (
-  <div className={styles.component}>
-    <h2>Tables view</h2>
-    <Link exact to={`${process.env.PUBLIC_URL}/booking/new`} >New Booking </Link>
-    <Link exact to={`${process.env.PUBLIC_URL}/booking/123abc`} >Booking 123abc </Link>
-    <Link exact to={`${process.env.PUBLIC_URL}/event/new`} >New Event </Link>
-    <Link exact to={`${process.env.PUBLIC_URL}/event/123abc`} >Event 123abc </Link>
-  </div>
+  <Paper className={styles.component}>
+    <Table>
+      <TableHead>
+        <TableRow>
+          <TableCell>Table</TableCell>
+          <TableCell>Status</TableCell>
+          <TableCell>Time from order</TableCell>
+          <TableCell>Action</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {demoContent.map(row => (
+          <TableRow key={row.id}>
+            <TableCell component="th" scope="row">
+              {row.id}
+            </TableCell>
+            <TableCell>
+              {row.status}
+            </TableCell>
+            <TableCell>
+              {row.order && (
+                <Button to={`${process.env.PUBLIC_URL}/waiter/order/${row.order}`}>
+                  {row.order}
+                </Button>
+              )}
+            </TableCell>
+            <TableCell>
+              {renderActions(row.status)}
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  </Paper>
 );
 
 export default Tables;
